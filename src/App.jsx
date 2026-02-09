640b437648ac2c02f510005cc1d159dc0503959d
kgcodex <hexcodex101@gmail.com>
2026-02-09 17:50:35 +0530

    feat:Completed Cocktail and About Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,7 +13,8 @@ const App = () => {
     <main className="no-scrollbar">
       <Navbar />
       <Hero />
-      <div className="h-dvh bg-black"></div>
+      <Cocktails />
+      <About />
     </main>
   );
 };
